ca87e4d2ff13095ffbd01d3c153dfc7d17b38015
n.giannouli <n.giannouli@gnomon.com.gr>
2024-07-16 13:09:31 +0300

    Update custom_searchable_dropdown.dart

--- lib/custom_searchable_dropdown.dart
+++ lib/custom_searchable_dropdown.dart
@@ -556,7 +556,7 @@ class _CustomSearchableDropDownState extends State<CustomSearchableDropDown>
             itemBuilder: (BuildContext context, int index) {
               return TextButton(
                 style: TextButton.styleFrom(
-                    backgroundColor: widget.primaryColor ?? Colors.black,
+                    backgroundColor: Colors.white,
                     padding: EdgeInsets.all(8),
                     tapTargetSize: MaterialTapTargetSize.shrinkWrap),
                 child: Padding(
